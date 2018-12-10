de5c4f5e3948294cb500e26d114d5e8818bd4df3
Marcio Katsumi Yamashita <marciokatsumiyamashita@gmail.com>
2018-12-10 09:26:05 -0200

    Update de client (executavel, json e script)

--- README.md
+++ README.md
@@ -20,13 +20,15 @@ Parâmetros de entrada fornecidos via .json no local do script/executável.
 - **ftp_client.exe**  
 Executável gerado via pyinstaller.  
 pyinstaller --onefile ftp_client.py  
-PyInstaller: 3.3  
-Python: 2.7.15  
+PyInstaller: 3.3.1  
+Python: 3.6.0
 Platform: Windows-7-6.1.7601-SP1  
 
 - **ftp_client.json**  
-json com informações:  
-    -Do servidor de FTP (e.g. hostname ou ip).  
-    -Do software de aquisição (e.g. DADAS ou SISMO).  
-    -Dos sensores habilitados para coleta.  
-    -Do path para download.  
+json de configuração:  
+    * O objeto é o servidor de FTP (e.g. hostname ou ip).  
+    * As propriedades do objeto são:  
+        - sync: #número de horas antes da atual.
+        - software: SISMO ou DADAS.
+        - ext: array de extensão dos arquivos.
+        - path: path de destino do arquivo.
--- ftp_client.py
+++ ftp_client.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+# PYTHON 3 compatible code (because using MLSD from ftplib)
+
 from ftplib import FTP
 import os
+import re
 import sys
 import json
+import jsonschema
 import getpass
 from datetime import datetime, timedelta
 
@@ -10,69 +14,36 @@ from datetime import datetime, timedelta
 class FTP_connection(object):
     """docstring for FTP_connection."""
 
-    def __init__(self, server_name=[], acqn_mode=[], path_target=os.getcwd()):
+    def __init__(self, server_name, acqn_mode):
         self._ftp = FTP('')
-        self.server_name = server_name
-        self.acqn_mode = acqn_mode
-        self.path_target = path_target
-        self.make_session()
+        self.time = datetime.utcnow()
+        self.server_name = server_name.upper()
+        self.acqn_mode = acqn_mode.upper()
+        self._ftp_session()
 
-    @property
-    def server_name(self):
-        return self._server_name
-
-    @server_name.setter
-    def server_name(self, d):
-        if not d:
-            raise Exception("server_name cannot be empty.")
-        if not isinstance(d, str):
-            raise Exception("server_name needs to be a string.")
-        self._server_name = d.upper()
-
-    @property
-    def acqn_mode(self):
-        return self._acqn_mode
-
-    @acqn_mode.setter
-    def acqn_mode(self, d):
-        if not d:
-            raise Exception("server_name cannot be empty.")
-        if not isinstance(d, str):
-            raise Exception("server_name needs to be a string.")
-        if d.upper() not in ['DADAS', 'SISMO']:
-            raise Exception('acqn_mode is DADAS or SISMO')
-        self._acqn_mode = d.upper()
-
-    def _file_dir(self, mode):
-        path = ('DADAS32\Bin\Output' if mode == 'DADAS'
+    def _ftp_session(self):
+        self._ftp.connect(self.server_name, 8602)
+        self._ftp.login('SABCSIS03', 'oceanop2')
+        print('FTP session opened @ {}'.format(self.server_name))
+        path = ('DADAS32\Bin\Output' if self.acqn_mode == 'DADAS'
                 else 'Program Files (x86)\SISMO\HIST')
         try:
             self._ftp.cwd(path)
-            self.file_dir = 'C:\\{}'.format(path)
-            print('Directory is {}'.format(self.file_dir))
+            self.lista = self.file_list()
+            print('Directory is C:\\{}'.format(path))
         except Exception:
             print('No such Directory C:\\{}'.format(path))
             self.quit()
 
     def file_list(self):
-        """Return a list of file names."""
+        """Returns a list of file names in a specified directory."""
         try:
-            nlst = self._ftp.nlst()
+            lista = self._ftp.nlst()
         except Exception:
             print('FTP Connection Timed Out/Closed.\n Trying to Reconnect.')
-            self.make_session()
-            nlst = self._ftp.nlst()
-        return nlst
-
-    def _ftp_session(self, server_name):
-        self._ftp.connect(server_name, 8602)
-        self._ftp.login('SABCSIS03', 'oceanop2')
-        print('FTP session opened @ {}'.format(server_name))
-
-    def make_session(self):
-        self._ftp_session(self.server_name)
-        self._file_dir(self.acqn_mode)
-        self.nlst = self.file_list()
+            self._ftp_session()
+            lista = self.file_list()
+        return lista
 
     def quit(self):
         """Close ftp connection."""
@@ -82,16 +53,37 @@ class FTP_connection(object):
             self._ftp.close()
         print('FTP session closed')
 
-    @staticmethod
-    def check_if_exists(filename, filelist):
-        return filename in filelist
+    def sync_files(self, sync, ext, path_target):
+        T = (self.time - timedelta(hours=sync + 1)).strftime('%Y-%m-%d-%H-45')
+        fmt = '(\d{4}-\d{2}-\d{2}-\d{2}-\d{2})'
+        get_ext = lambda x: x.split('.')[-1]        
+        for i in self.lista:
+            value = re.search(fmt, i)
+            if value:
+                if (value.group(0) > T) & (get_ext(i) in ext):
+                    self.download_file(i, path_target)
 
-    def download_file(self, filename, path_target=os.getcwd()):
-        if self.check_if_exists(filename, self.nlst):
-            self.path_target = path_target
-            self._retrieve(filename)
+    def filter_time(self, x, time):
+        fmt = '(\d{4}-9\d{2}-\d{2}-\d{2}-\d{2})'
+        Tfmt = time.strftime('%Y-%m-%d-%H-45')
+        value = re.search(fmt, x)
+        if value:
+            if value.group(0) > Tfmt:
+                return True
+        return False
+
+    def download_file(self, filename=None, path_target=os.getcwd()):
+        """Download filename if exists @ server."""
+        self.test_path_target(path_target)
+        if filename:
+            if filename in self.lista:
+                self._retrieve(filename)
+            else:
+                print('{} does not exist @ FTP server'.format(filename))
+                return
         else:
-            print('{} does not exist @ FTP server'.format(filename))
+            print('Filename is not defined')
+            return
 
     def _retrieve(self, filename):
         file_ = os.path.join(self.path_target, filename)
@@ -99,28 +91,22 @@ class FTP_connection(object):
         try:
             self._ftp.retrbinary('RETR ' + filename, target.write, 1024)
         except Exception:
-            print('FTP Connection Timed Out/Closed.\n Trying to Reconnect.')
-            self.make_session()
+            print('FTP Connection Timed Out/Closed.\n Trying to Reconnect...')
+            self._ftp_session()
             self._ftp.retrbinary('RETR ' + filename, target.write, 1024)
         target.close()
         print('Download {} @ {}'.format(filename, self.path_target))
 
-    @property
-    def path_target(self):
-        return self._path_target
-
-    @path_target.setter
-    def path_target(self, d):
-        if not d:
-            raise Exception("path_target cannot be None.")
-        if not isinstance(d, str):
-            raise Exception("path_target needs to be a string.")
+    def test_path_target(self, d):
         if not os.path.isdir(d):
-            raise Exception('path_target refers to a not valid path.')
+            print('path_target refers to a not valid path.')
+            self.path_target = os.getcwd()       
         if not self._isWritable(d):
-            raise Exception('{} does not have privileges to write'
-                            ' @ {}.'.format(getpass.getuser(), d))
-        self._path_target = d
+            print('{} does not have privileges to write'
+                  ' @ {}'.format(getpass.getuser(), d))
+            self.path_target = os.getcwd()       
+        else:
+            self.path_target = d
 
     def _isWritable(self, path):
         try:
@@ -132,80 +118,33 @@ class FTP_connection(object):
         return True
 
 
-def create_filename(dt, **kw):
-    files = []
-    check_dict(kw)
-    if kw['software'] == 'SISMO':
-        for i in kw['sensors']:
-            if i == 'young':
-                files.append('{}01_{:{fmt}}.yng_gz'.format(
-                    kw['id'], dt - timedelta(hours=1),
-                    fmt="%Y-%m-%d-%H-50"))
-            if i == 'fsi2d':
-                files.append('{}03_{:{fmt}}.fsi_gz'.format(
-                    kw['id'], dt - timedelta(hours=1),
-                    fmt="%Y-%m-%d-%H-50"))
-            if i == 'fsi3d':
-                files.append('{}04_{:{fmt}}.fsi_gz'.format(
-                    kw['id'], dt - timedelta(hours=1),
-                    fmt="%Y-%m-%d-%H-40"))
-            if i == 'adcp':
-                files.append('{}02_{:{fmt}}.adc_gz'.format(
-                    kw['id'], dt - timedelta(hours=1),
-                    fmt="%Y-%m-%d-%H-50"))                    
-    if kw['software'] == 'DADAS':
-        for i in kw['sensors']:
-            if i == 'young':
-                files.append('{}01_{:{fmt}}.yng_gz'.format(
-                    kw['id'], dt, fmt="%Y-%m-%d-%H-00"))
-            if i == 'fsi2d':
-                files.append('{}03_{:{fmt}}.fsi_gz'.format(
-                    kw['id'], dt, fmt="%Y-%m-%d-%H-00"))
-            if i == 'fsi3d':
-                files.append('{}04_{:{fmt}}.fsi_gz'.format(
-                    kw['id'], dt, fmt="%Y-%m-%d-%H-00"))
-            if i == 'adcp':
-                files.append('{}02_{:{fmt}}.adc_gz'.format(
-                    kw['id'], dt, fmt="%Y-%m-%d-%H-00"))                    
-    return files
-
-
-def check_dict(kw):
-    if 'id' not in kw:
-        raise Exception("'id' not found.")
-    if 'sensors' not in kw:
-        raise Exception("'sensors' not found.")
-    if 'software' not in kw:
-        raise Exception("'software' not found.")
-
-
 def getparams(dname):
     """Load a json file in same path of script. """
-    with open(os.path.join(dname, 'ftp_client.json')) as f:
-        params = json_load_byteified(f)
-        return params
-
-
-def json_load_byteified(file_handle):
-    return _byteify(json.load(file_handle, object_hook=_byteify),
-                    ignore_dicts=True)
-
-
-def _byteify(data, ignore_dicts=False):
-    # if this is a unicode string, return its string representation
-    if isinstance(data, unicode):
-        return data.encode('utf-8')
-    # if this is a list of values, return list of byteified values
-    if isinstance(data, list):
-        return [_byteify(item, ignore_dicts=True) for item in data]
-    # if this is a dictionary, return dictionary of byteified keys and values
-    # but only if we haven't already byteified it
-    if isinstance(data, dict) and not ignore_dicts:
-        return {_byteify(key, ignore_dicts=True):
-                _byteify(value, ignore_dicts=True)
-                for key, value in data.iteritems()}
-    # if it's anything else, return it in its original form
-    return data
+    schema = {
+        "$schema": "http://json-schema.org/draft-06/schema#",
+        "type": "object",
+        "additionalProperties": {"$ref": "#/definitions/FTPClientValue"},
+        "definitions": {
+            "FTPClientValue": {
+                "type": "object",
+                "additionalProperties": True,
+                "properties": {
+                    "sync": {"type": "integer"},
+                    "software": {"type": "string"},
+                    "ext": {"type": "array", "items": {"type": "string"}},
+                    "path": {"type": "string"},},
+                "required": ["ext", "path", "software", "sync"],
+                "title": "FTPClientValue"}}}
+    try:
+        with open(os.path.join(dname, 'ftp_client.json')) as f:
+            params = json.load(f)
+    except Exception:
+        raise Exception("Poorly-formed text, not JSON:")
+    try:
+        jsonschema.validate(params, schema)
+    except jsonschema.exceptions.ValidationError as e:
+        raise Exception("Well-formed but invalid JSON:", e)
+    return params
 
 
 if __name__ == '__main__':
@@ -215,14 +154,14 @@ if __name__ == '__main__':
     else:
         dname = os.path.dirname(os.path.abspath(__file__))
 
-    dt = datetime.utcnow()
     ftp_servers = getparams(dname)
+
     for i in ftp_servers:
+        print('Tentativa de conexão em {}'.format(i))
         kw = ftp_servers[i]
         try:
-            server = FTP_connection(i, kw['software'], str(dname))
-            files = create_filename(dt, **kw)
-            [server.download_file(j, kw['path']) for j in files]
+            server = FTP_connection(i, kw['software'])
+            server.sync_files(kw['sync'], kw['ext'], kw['path'])
             server.quit()
         except Exception:
             print('servidor sem conexão em {}'.format(i))
